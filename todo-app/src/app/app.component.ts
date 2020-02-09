4fe02b906713138cfeb5eb6afeaed825a017fe37
ryosk7 <uchiryo7@gmail.com>
2020-02-09 19:11:37 +0900

    feat(client): commentを返す

--- todo-app/src/app/app.component.ts
+++ todo-app/src/app/app.component.ts
@@ -8,10 +8,18 @@ import { HttpClient } from '@angular/common/http';
 })
 export class AppComponent {
   title = 'todo-app';
+  commentsRef;
   comments;
 
   constructor(private http: HttpClient) {
     http.get('http://localhost:3000/comments.json')
       .subscribe(res => this.comments = res);
+  }
+
+  addCommentData(content: string, initial: string, username: string) {
+    this.commentsRef.add(
+      {
+        content: this.comments.value,
+      });
   }
 }
